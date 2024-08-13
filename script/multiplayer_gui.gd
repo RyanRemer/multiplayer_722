78658e0f1719fcfbbd80dac28d0a18ef77187596
Ryan Remer <ryandremer@gmail.com>
2024-08-12 21:48:48 -0600

    Basic packet sending

--- script/multiplayer_gui.gd
+++ script/multiplayer_gui.gd
@@ -2,19 +2,25 @@ extends Panel
 @onready var message_label = $VBoxContainer/MessageLabel
 
 var server_address = "localhost";
-var count = 0;
+var packet = {
+	"count" : 0,
+}
 
 func _ready():
+	packet["sender"] = randi();
 	multiplayer.server_disconnected.connect(_server_disconnected);
 	
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		count += 1;
-		multiplayer.multiplayer_peer.put_var(count);
-		
-	if multiplayer.is_server() and multiplayer.multiplayer_peer.get_available_packet_count() > 0:
-		var count = multiplayer.multiplayer_peer.get_var();
-		print(str(multiplayer.get_unique_id()) +  ": Got count " + str(count));
+		packet["count"] += 1;
+		var serialize = var_to_str(packet);
+		_get_packet.rpc_id(1, serialize);
+
+@rpc("any_peer")
+func _get_packet(packet):
+	var data = str_to_var(packet);
+	print(str(multiplayer.get_unique_id()) + ": Recieved " + str(data) + " from " + str(multiplayer.get_remote_sender_id()))
+	print(multiplayer.get_peers())
 
 func _server_disconnected():
 	message_label.text = "Disconnected";
@@ -25,7 +31,8 @@ func _on_host_button_pressed():
 	var peer = ENetMultiplayerPeer.new();
 	var error = peer.create_server(5656);
 	if error != OK:
-		message_label.text = "Error: " + error.message_text;
+		message_label.text = "Error: " + str(error.message_text);
+		return;
 	
 	multiplayer.multiplayer_peer = peer;
 	message_label.text = "";
@@ -38,7 +45,8 @@ func _on_join_container_pressed():
 	var peer = ENetMultiplayerPeer.new();
 	var error = peer.create_client(server_address, 5656);
 	if error != OK:
-		message_label.text = "Error: " + error.message_text;
+		message_label.text = "Error: " + str(error);
+		return;
 		
 	multiplayer.multiplayer_peer = peer;
 	message_label.text = "Connected to Server";
